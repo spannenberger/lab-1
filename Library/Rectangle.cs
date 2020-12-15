935a4037093eb79f5b88476ce22b460e7a2d2795
Igor Terekhin <terekhin01@mail.ru>
2020-12-15 12:32:07 +0500

    full

---
+++ Library/Rectangle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+
+namespace Library
+{
+    public class Rectangle
+    {
+        private PointF P0;
+        private PointF P1;
+
+        private float A
+        {
+            get
+            {
+                return P1.X - P0.X;
+            }
+        }
+
+        private float B
+        {
+            get
+            {
+                return P1.X - P0.Y;
+            }
+        }
+
+
+        #region Ctors
+        public Rectangle(PointF d0, PointF d1)
+        {
+            if (d0.X > d1.X || d0.Y > d1.Y)
+            {
+                throw new ArgumentException("dot0 coors must be greater or equeal than dot1 coors");
+            }
+            P0 = d0;
+            P1 = d1;
+        }
+        public Rectangle(float x0, float y0, float x1, float y1)
+            : this(new PointF(x0, y0), new PointF(x1, y1))
+        {
+
+        }
+        #endregion
+
+        public Rectangle Scale(float scale_x = 1.0f, float scale_y = 1.0f)
+        {
+            if (scale_x <= 0 || scale_y <= 0)
+            {
+                throw new ArgumentException("Scale must be greater 0");
+            }
+            var newD1X = P0.X + A * scale_x;
+            var newD1Y = P0.Y + B * scale_y;
+            return new Rectangle(new PointF(P0.X, P0.Y), new PointF(newD1X, newD1Y));
+        }
+
+        public Rectangle Scale(float scale)
+        {
+            return Scale(scale, scale);
+        }
+
+        public Rectangle Move(float length_x, float length_y)
+        {
+            var tmpP0 = new PointF(P0.X + length_x, P0.Y + length_y);
+            var tmpP1 = new PointF(P1.X + length_x, P1.Y + length_y);
+            return new Rectangle(tmpP0,tmpP1);
+        }
+
+        public Rectangle Min(Rectangle rect)
+        {
+            var tmpX0 = Math.Min(P0.X, rect.P0.X);
+            var tmpY0 = Math.Min(P0.Y, rect.P0.Y);
+            var tmpX1 = Math.Max(P1.X, rect.P1.X);
+            var tmpY1 = Math.Max(P1.Y, rect.P1.Y);
+            return new Rectangle(tmpX0, tmpY0, tmpX1, tmpY1);
+        }
+
+        public Rectangle Intersection(Rectangle rect)
+        {
+            var tmpX0 = Math.Max(P0.X, rect.P0.X);
+            var tmpY0 = Math.Max(P0.Y, rect.P0.Y);
+            var tmpX1 = Math.Min(P1.X, rect.P1.X);
+            var tmpY1 = Math.Min(P1.Y, rect.P1.Y);
+            try
+            {
+                return new Rectangle(tmpX0, tmpY0, tmpX1, tmpY1);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"x0={P0.X}\tA={A}\ny0={P0.Y}\tB={B}";
+        }
+    }
+}
---
+++ lab№1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Library;
+using System.Linq;
+
+namespace Spannenberger_s_lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rectangle> Rectangles = new List<Rectangle>();
+            PrintHelp();
+            while (Run(Rectangles)) ;
+        }
+
+        static bool Run(List<Rectangle> Rectangles)
+        {
+            string buf;
+            string[] command;
+            buf = Console.ReadLine();
+            command = buf.Split(' ');
+            try
+            {
+                switch (command[0].ToLower())
+                {
+                    case "stop":
+                        return false;
+
+                    case "new":
+                        AddNewRectangle(Rectangles, command);
+                        break;
+
+                    case "min":
+                        GetMinRectangle(Rectangles, command);
+                        break;
+
+                    case "int":
+                        GetIntersection(Rectangles, command);
+                        break;
+
+                    case "scale":
+                        ScaleRectangle(Rectangles, command);
+                        break;
+
+                    case "move":
+                        MoveRectangle(Rectangles, command);
+                        break;
+
+                    case "print":
+                        PrintRectangles(Rectangles);
+                        break;
+
+                    case "help":
+                        PrintHelp();
+                        break;
+
+                    default:
+                        Console.WriteLine("Комманда некорректна");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Комманда некорректна:{e?.Message}");
+            }
+            return true;
+        }
+
+        static void GetIntersection(List<Rectangle> Rectangles, string[] command)
+        {
+            Rectangles.Add(Rectangles[Convert.ToInt32(command[1])].Intersection(Rectangles[Convert.ToInt32(command[2])]));
+            Console.WriteLine(Rectangles.Last());
+        }
+
+        static void GetMinRectangle(List<Rectangle> Rectangles, string[] command)
+        {
+            Rectangles.Add(Rectangles[Convert.ToInt32(command[1])].Min(Rectangles[Convert.ToInt32(command[2])]));
+            Console.WriteLine(Rectangles.Last());
+        }
+
+        static void AddNewRectangle(List<Rectangle> Rectangles, string[] command)
+        {
+            var tmp_args = Parse(command);
+            Rectangles.Add(new Rectangle(tmp_args[0], tmp_args[1], tmp_args[2], tmp_args[3]));
+        }
+
+        static void ScaleRectangle(List<Rectangle> Rectangles, string[] command)
+        {
+            Rectangles.Add(
+                Rectangles[Convert.ToInt32(command[1])]
+                .Scale(
+                    Convert.ToSingle(command[2]),
+                    Convert.ToInt32(command[3])
+                )
+            );
+        }
+
+        static void MoveRectangle(List<Rectangle> Rectangles, string[] command)
+        {
+            Rectangles.Add(
+                Rectangles[Convert.ToInt32(command[1])]
+                .Move(
+                    Convert.ToInt32(command[2]),
+                    Convert.ToInt32(command[3])
+                )
+            );
+        }
+
+        static void PrintRectangles(List<Rectangle> Rectangles)
+        {
+            for (int i = 0; i < Rectangles.Count; i++)
+            {
+                Console.WriteLine($"rect №{i}\n{Rectangles[i]}");
+                Console.WriteLine("---------------------------------");
+            }
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("Списко доступных комманд:" +
+                "\nnew x0 y0 x1 y1 - создает новый прямоугольник с началом в точке {x0, y0} и с концом в точке {x1, y2}" +
+                "\nmove rect_number x y - создает копию прямоугольника под номером rect_number и двигает её на x единиц по Ox, y по Oy" +
+                "\nscale rect_number x y - создает копию прямоугольника под номером rect_number и увеличивает/уменьшает её в х раз по Ох, у по Оу" +
+                "\nmin rect_number1 rect_number2 - создает наименьший прямоугольник, содержащий два заданных прямоугольника" +
+                "\nint rect_number1 rect_number2 - создает прямоугольник, являющийся общей частью (пересечением) двух заданых прямоугольников" +
+                "\nprint - печатает все прямоугольники" +
+                "\nhelp - печатает список комманд" +
+                "\nstop - заканчивает программу");
+        }
+
+        static float[] Parse(string[] coor)
+        {
+            var res = new float[coor.Length - 1];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = Convert.ToSingle(coor[i + 1]);
+            }
+            return res;
+        }
+    }
+}
